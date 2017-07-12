b4b18ecc2054dcbf9a5d7e2f34effde98f0e144a
rehan <mohd.rehan@affle.com>
2017-07-12 20:37:30 +0530

    updated

---
+++ Services/provider.js
@@ -0,0 +1,38 @@
+var crypto= require('crypto');
+var providerModel= require('../Model/provider');
+var unique_key_for_login= "abcde12345123456";
+
+/* this function is used for login customer by email/mobile and password */
+var login = (data, callback) => {
+
+        /* function to encrypt the password we entered so that we can match it with the encrypted password saved in database */
+        function encrypt(password){
+            var cypher=  crypto.createCipher('aes-256-ctr','password');
+            var crypted= cypher.update(password,'utf8','hex');
+            crypted+= cypher.final('hex');
+            return crypted;
+        }
+        data.password= encrypt(data.password);
+
+        // query to find the user with email/mobile and password
+
+        providerModel.userModel.findOne({$or : [{"email" : data.email, "password": data.password} ,
+         {"mobile": data.mobile, "password" :data.password}]}, (err, info) => {
+            if(err){
+                callback(err)
+            }
+            // when email/mobile and password values are present in database
+            else if(info!= null){
+                callback(" you are logged in \n " +info);
+            }
+            // when data doesn't match with any document in database
+            else if(info== null){
+                callback("wrong email id or password");
+            }
+        });
+}
+
+module.exports= {
+    login      :   login,
+    unique_key_for_login : unique_key_for_login
+}
